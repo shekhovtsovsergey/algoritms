82df1a8ca9b1d6c832969b5a48bf042068859ce8
shekhovtsovsergey <89629417575@mail.ru>
2023-09-10 23:23:20 +0300

    Lesson2

--- src/main/java/org/example/Lesson2/SortServiceImpl.java
+++ src/main/java/org/example/Lesson2/SortServiceImpl.java
@@ -6,33 +6,31 @@ public class SortServiceImpl {
 
     public void sort(Notebook[] notebooks){
 
-        boolean isChange = false;
+        boolean isChange = true;
         int limit = notebooks.length;
 
-        while (isChange){
+        while (isChange==true){
             limit--;
             isChange = false;
-            for (int i = 0; i < notebooks.length; i++) {
-                if (notebooks[i].getPrice() > notebooks[i+1].getPrice()){
+            for (int i = 0; i < notebooks.length-1; i++) {
+                if (notebooks[i].getManufacturer().compareTo(notebooks[i+1].getManufacturer()) > 0){
                     swap(notebooks,i);
                     isChange = true;
                 } else if (notebooks[i].getRam() > notebooks[i+1].getRam()) {
                     swap(notebooks,i);
                     isChange = true;
-                } else if (notebooks[i].getManufacturer().compareTo(notebooks[i+1].getManufacturer()) > 0){
+                } else if (notebooks[i].getPrice() > notebooks[i+1].getPrice()){
                     swap(notebooks,i);
                     isChange = true;
                 }
             }
-        }
             System.out.println(limit + " " + Arrays.toString(notebooks));
+        }
     }
 
-
-    public void  swap(Notebook[] notebooks, int i){
+    public void swap(Notebook[] notebooks, int i){
         Notebook temp = notebooks[i];
         notebooks[i] = notebooks[i + 1];
         notebooks[i + 1] = temp;
     }
-
 }
